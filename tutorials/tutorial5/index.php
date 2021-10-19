f493333e1e9f4cf4b8ffa04d224f6fa9a693f9bb
winngwephyo-tech <ngwephyowin@gmail.com>
2021-10-19 15:46:16 +0630

    tuto5

---
+++ tutorials/tutorial5/index.php
@@ -0,0 +1,55 @@
+<?php
+$txtFile = 'file\file1.txt';
+$excelFile = 'file\file2.excel';
+$csvFile = 'file\file3.csv';
+$docFile = 'file\file4.doc';
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tutorial5 _ Read ALL File</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+  <div class="accordion_container">
+    <div class="accordion_head">Text File<span class="plusminus">+</span></div>
+    <div class="accordion_body" style="display: none;">
+      <p><?php
+          echo file_get_contents($txtFile);
+          ?>
+      </p>
+    </div>
+    <div class="accordion_head">Excel File<span class="plusminus">+</span></div>
+    <div class="accordion_body" style="display: none;">
+      <p><?php
+          echo file_get_contents($excelFile);
+          ?>
+      </p>
+    </div>
+    <div class="accordion_head">CSV File<span class="plusminus">+</span></div>
+    <div class="accordion_body" style="display: none;">
+      <p><?php
+          echo file_get_contents($csvFile);
+          ?>
+      </p>
+    </div>
+    <div class="accordion_head">Doc File<span class="plusminus">+</span></div>
+    <div class="accordion_body" style="display: none;">
+      <p><?php
+          echo file_get_contents($docFile);
+          ?>
+      </p>
+    </div>
+
+  </div>
+  <script src="js/library/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="js/accordion.js"></script>
+</body>
+
+</html>
